f460bc8e9802b768451e63d1458a99875184c122
V4IDIK <vaidik2001@gmail.com>
2024-08-29 16:15:03 +0530

    OverLoadnRide

---
+++ Over_loadnride.java
@@ -0,0 +1,34 @@
+package lab;
+
+class overloading{
+    public int add(int x, int y) {
+
+        return x+y;
+    }
+    
+    public int add(int x, int y, int z){
+    
+        return x+y+z;
+    }
+}
+class Override extends overloading{
+
+    public int add(int x, int y){
+
+        return x-y;
+    }
+}
+
+public class Over_loadnride {
+    public static void main(String[] args) {
+        
+        overloading o=new overloading();
+        Override oo=new Override();
+        System.out.println(o.add(1, 2));
+        System.out.println(o.add(1, 2, 3));
+        System.out.println(oo.add(1, 2));
+    }
+}
+
+
+
