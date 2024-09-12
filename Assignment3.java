41192d9e505788405cf9bc8c53a2ced26c6342b0
V4IDIK <vaidik2001@gmail.com>
2024-09-12 15:41:46 +0530

    Assignment3 given in class

---
+++ Assignment3.java
@@ -0,0 +1,20 @@
+package lab;
+
+class CLASS {
+    String Name;
+    
+    public CLASS(String Name) {
+        
+        this.Name = Name;
+        System.out.println("Name : " +Name);
+    }
+}
+
+public class Assignment3 {
+    public static void main(String[] args) {
+        
+        new CLASS("VAIDIK");
+        
+    }
+}
+
